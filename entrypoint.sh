53489be315834c5ad55dbd4dd94efacc38ea25ba
cixi1818 <cixi1818@gmail.com>
2018-10-07 17:45:15 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 cd /goflyway-heroku
 
-wget -O goflyway.tar.gz https://github.com/coyove/goflyway/releases/download/v1.2.0a/goflyway_linux_amd64.tar.gz
+wget -O goflyway.tar.gz https://github.com/coyove/goflyway/releases/download/v1.3.0a/goflyway_linux_amd64.tar.gz
 tar -zxf goflyway.tar.gz
 #cd /goflyway/goflyway_linux_amd64
 chmod +x goflyway 
